67af707c4fdcaac976ddca2a8a6e6537f582d017
glhrmv <gvmgui@gmail.com>
2017-10-29 10:51:42 +0000

    Fix destructuring assignments

--- bot.js
+++ bot.js
@@ -1,48 +1,48 @@
-const axios = require('axios');
-const keys = require('./keys');
+const axios = require('axios')
+const keys = require('./keys')
 
-const Discordie = require('discordie');
-const Events = Discordie.Events;
-const client = new Discordie();
+const Discordie = require('discordie')
+const { Events } = Discordie
+const client = new Discordie()
 
 // private keys
-const { discordToken, lastfmApiKey } = keys.discordToken;
+const { discordToken, lastfmApiKey } = keys
 
 // last.fm API endpoint
-const apiUrl = 'http://ws.audioscrobbler.com/2.0/?method=';
+const apiUrl = 'http://ws.audioscrobbler.com/2.0/?method='
 
 // connect to Discrd
-client.connect({ token: discordToken });
+client.connect({ token: discordToken })
 
 client.Dispatcher
   .on(Events.GATEWAY_READY, (e) => {
-    console.log('Connected as: ' +  client.User.username);
+    console.log('Connected as: ' +  client.User.username)
   })
   .on(Events.MESSAGE_CREATE, (e) => {
-    let input = e.message.content.split(' ');
+    let input = e.message.content.split(' ')
 
     if (input.length === 2) {
         if ( input[0] === ".np") {
-            nowPlaying(input[1], e);
+            nowPlaying(input[1], e)
         } else if ( input[0] === ".topalbums") {
-            topAlbums(input[1], e);
+            topAlbums(input[1], e)
         }
     }
   });
 
 // commands
 const nowPlaying = (user, e) => {
-    let method = 'user.getRecentTracks';
-    let qs = '&user=${user}&api_key=${lastfmApiKey}&limit=2&format=json';
+    let method = 'user.getRecentTracks'
+    let qs = '&user=${user}&api_key=${lastfmApiKey}&limit=2&format=json'
 
-    let reqUrl = `${apiUrl}${method}${qs}`;
+    let reqUrl = `${apiUrl}${method}${qs}`
 
     axios.get(reqUrl)
       .then((res) => {
-        let latestTrack = res.data.recenttracks.track[0];
-        console.log(latestTrack);
-        let latestTrackName = latestTrack.name;
-        let latestTrackArtist = latestTrack.artist['#text'];
+        let latestTrack = res.data.recenttracks.track[0]
+
+        let { name } = latestTrack
+        let { '#text': artist } = latestTrack.artist;
         e.message.channel.sendMessage(`currently listening to: ${latestTrackArtist} - ${latestTrackName}`);
       })
       .catch((err) => {
