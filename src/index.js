fb8e01aeef6657c7e901945462de24af26d0f290
Guilherme Vale <oguilhermevale@gmail.com>
2021-11-02 20:07:45 +0000

    Get token from .env file

--- src/index.js
+++ src/index.js
@@ -1,102 +1,11 @@
-const Discord = require('discord.js')
-const axios = require('axios')
+const { Client, Intents } = require('discord.js');
+const envy = require('envy');
+const env = envy();
 
-// Private keys defined in the keys.js file
-const { DISCORD_BOT_TOKEN, LASTFM_API_KEY } = require('./keys')
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// Last.fm API endpoint
-const LASTFM_API_URL = 'http://ws.audioscrobbler.com/2.0/?method='
+client.once('ready', () => {
+	console.log('Ready!');
+});
 
-// Create a new Discord client
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-
-// Event listener for messages
-client.on('message', message => {
-  const msg = message.content.split(' ')
-
-  if (msg.length === 2) {
-    if (msg[0] === '.nowplaying') {
-      now_playing(msg[1], message)
-    } else if (msg[0] === '.topalbums') {
-      top_albums(msg[1], message)
-    }
-  }
-})
-
-// Bot commands
-const now_playing = (user, message) => {
-  const METHOD = 'user.getRecentTracks'
-  const QUERY_STRING = `&user=${user}&api_key=${LASTFM_API_KEY}&limit=2&format=json`
-
-  const request_url = `${LASTFM_API_URL}${METHOD}${QUERY_STRING}`
-
-  axios
-    .get(request_url)
-    .then(res => {
-      if (res.data.message) {
-        message.channel.send('User not found')
-        return
-      }
-
-      const latest_track = res.data.recenttracks.track[0]
-
-      if (!latest_track) {
-        e.message.channel.send('User not found')
-        return
-      }
-
-      const {
-        name,
-        artist: { '#text': artist },
-      } = latest_track
-
-      message.channel.send(`${user} is currently listening to: ${name} - ${artist}`)
-    })
-    .catch(err => {
-      console.log('Got an error:', err)
-    })
-}
-
-const top_albums = (user, message) => {
-  const METHOD = 'user.getTopAlbums'
-  const QUERY_STRING = `&user=${user}&api_key=${LASTFM_API_KEY}&limit=3&format=json`
-
-  const request_url = `${LASTFM_API_URL}${METHOD}${QUERY_STRING}`
-
-  axios
-    .get(request_url)
-    .then(res => {
-      const top_albums = res.data.topalbums
-
-      if (top_albums) {
-        if (top_albums.album[0]) {
-          let response = `${user}'s top albums (all time):\n`
-
-          for (let i = 0; i < 3; i++) {
-            const {
-              artist: { name: album_artist },
-              name: album_title,
-            } = top_albums.album[i]
-
-            response += `${i + 1}: ${album_artist} - ${album_title}\n`
-          }
-
-          message.channel.send(response)
-        } else {
-          message.channel.send("That user hasn't listened to any music in this period")
-        }
-      } else {
-        message.channel.send('Iinvalid user')
-      }
-    })
-    .catch(err => {
-      console.log('Got an error:', err)
-    })
-}
-
-// Log in to Discrd
-client.login(DISCORD_BOT_TOKEN)
+client.login(env.token);
